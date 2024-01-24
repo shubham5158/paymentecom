9128215ce42e3c83a37e91ef2e3a4d0ab36e97de
shubham <shubhamgogawale1@gmail.com>
2024-01-24 13:41:13 +0530

    first commit

---
+++ src/main/java/com/ecom/payment/PaymentecomApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentecomApplication.class, args);
+	}
+
+}
